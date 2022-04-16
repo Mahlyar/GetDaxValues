37189ad3019fc9d0af6dd0bdae0e96a1e88d32fc
malyar <mahlyar.ahmadsay@outlook.de>
2022-04-16 14:23:31 +0200

    created max and min methods

--- src/main/DAX/main.java
+++ src/main/DAX/main.java
@@ -1,29 +1,46 @@
 package main.DAX;
 
+import org.apache.log4j.BasicConfigurator;
 import yahoofinance.Stock;
 import yahoofinance.YahooFinance;
 import java.io.IOException;
+import java.math.RoundingMode;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Objects;
 //https://github.com/sstrickx/yahoofinance-api
+
 
 public class main {
     public static void main(String[] args) throws IOException {
+
+        BasicConfigurator.configure();
 
         boolean aktiv = true;
         Stock stock = YahooFinance.get("^GDAXI");
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");
         ArrayList<BigDecimal> DaxValues = new ArrayList<>();
 
+
+        System.out.println("------------------------------------");
         //calling the Daxprice every minute
         while (aktiv){
-            System.out.println(dtf.format(LocalDateTime.now()) + ": " + stock.getQuote().getPrice() + " €");
+            System.out.println("Time: " + dtf.format(LocalDateTime.now()));
+            System.out.println("Current price: " + stock.getQuote().getPrice() + " €");
             DaxValues.add(stock.getQuote().getPrice());
-            BigDecimal avg = DaxGetAvg(DaxValues);
-            System.out.println(avg);
 
+            BigDecimal avg = DaxGetAvg(DaxValues);
+            System.out.println("Average price: " + avg + " €");
+
+            BigDecimal maxPrice = DaxMaxPrice(DaxValues);
+            System.out.println("Max price: " + maxPrice + " €");
+
+            BigDecimal minPrice = DaxMinPrice(DaxValues);
+            System.out.println("Min price: " + minPrice + " €");
+            System.out.println("------------------------------------");
 
             try {
                 Thread.sleep(1*60*1000);
@@ -33,12 +50,22 @@ public class main {
         }
     }
 
-    private static BigDecimal DaxGetAvg(ArrayList<BigDecimal> daxValues) {
-        BigDecimal avg = null;
-        //todo: avg ermitteln
-                return avg;
 
+    private static BigDecimal DaxMinPrice(ArrayList<BigDecimal> daxValues) {
+        return Collections.min(daxValues);
     }
 
+    private static BigDecimal DaxMaxPrice(ArrayList<BigDecimal> daxValues) {
+        return Collections.max(daxValues);
+    }
+
+    private static BigDecimal DaxGetAvg(ArrayList<BigDecimal> daxValues) {
+
+        BigDecimal sum = daxValues.stream()
+                .map(Objects::requireNonNull)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+        return sum.divide(new BigDecimal(daxValues.size()),2, RoundingMode.CEILING);
+
+    }
 }
 
